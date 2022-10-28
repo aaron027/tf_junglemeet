7561ede752e3e2003c70f1901ce92689f02abb0a
Bo Chen <aaron027@Bos-MacBook-Air.gateway>
2022-10-28 23:08:09 +1100

    modulize files

--- frontend/cloudfront.tf
+++ frontend/cloudfront.tf
@@ -1,6 +1,4 @@
-
 # Cloudfront distribution for main s3 site.
-
 resource "aws_cloudfront_distribution" "www_s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.www_bucket.bucket_regional_domain_name
@@ -56,115 +54,8 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
   tags = var.common_tags
 
 }
-
-# Cloudfront S3 for redirect to www.
-resource "aws_cloudfront_distribution" "root_s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.root_bucket.bucket
-    origin_id   = "S3--.${var.bucket_name}"
-    custom_origin_config {
-      http_port = 80
-      https_port = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-  }
-
-  enabled         = true
-  is_ipv6_enabled = true
-
-  aliases = [var.domain_name]
-
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3--.${var.bucket_name}"
-
-    forwarded_values {
-      query_string = true
-
-      cookies {
-        forward = "none"
-      }
-
-      headers = ["Origin"]
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
- # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "S3--.${var.bucket_name}"
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-
-  price_class = "PriceClass_200"
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.1_2016"
-  }
-
-  tags = var.common_tags
-
-}
-
 resource "aws_cloudfront_origin_access_identity" "cloudfront_origin_access_identity" {
   comment    = "Only This User is allowed for S3 Read bucket"
-  depends_on = [time_sleep.wait_30_seconds]
-}
-
-
-
-data "aws_iam_policy_document" "root-cloudfront-read_bucket_only" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.root_bucket.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.root_bucket.arn]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
-    }
-  }
   depends_on = [time_sleep.wait_30_seconds]
 }
 
@@ -191,11 +82,7 @@ data "aws_iam_policy_document" "www-cloudfront-read_bucket_only" {
   depends_on = [time_sleep.wait_30_seconds]
 }
 
-resource "aws_s3_bucket_policy" "root_cloudfront_bucket" {
-  bucket     = aws_s3_bucket.root_bucket.id
-  policy     = data.aws_iam_policy_document.root-cloudfront-read_bucket_only.json
-  depends_on = [aws_cloudfront_distribution.root_s3_distribution]
-}
+
 resource "aws_s3_bucket_policy" "www_cloudfront_bucket" {
   bucket     = aws_s3_bucket.www_bucket.id
   policy     = data.aws_iam_policy_document.www-cloudfront-read_bucket_only.json
@@ -216,48 +103,6 @@ resource "time_sleep" "wait_60_seconds" {
 output "www_regional_name" {
   value = aws_s3_bucket.www_bucket.bucket_regional_domain_name
 }
-output "root_regional_name" {
-  value = aws_s3_bucket.root_bucket.bucket_regional_domain_name
-}
 
-output "identity-path" {
-  value = aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.cloudfront_access_identity_path
-}
 
-output "identity_arn" {
-  value = aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn
-}
 
-output "bucket_name" {
-  value = aws_s3_bucket.root_bucket.bucket
-}
-
-output "bucket_name_id" {
-  value = aws_s3_bucket.root_bucket.id
-}
-
-resource "aws_s3_bucket_public_access_block" "root_bucket_public_access_block" {
-  bucket = aws_s3_bucket.root_bucket.bucket
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  depends_on = [
-    aws_route53_record.root-a
-  ]
-}
-
-resource "aws_s3_bucket_public_access_block" "www_bucket_public_access_block" {
-  bucket = aws_s3_bucket.www_bucket.bucket
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  depends_on = [
-    aws_route53_record.www-a
-  ]
-}
