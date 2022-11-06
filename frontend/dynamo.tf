215cbfcbcaa371488d8ea08f6006d0a71cf32fd1
aaron027 <87021910+aaron027@users.noreply.github.com>
2022-11-06 14:57:38 +1100

    Update dynamo.tf

--- frontend/dynamo.tf
+++ frontend/dynamo.tf
@@ -1,11 +1,11 @@
-resource "aws_dynamodb_table" "terraform-statelock-frontend" {
-  name           = "terraform-statelock-frontend"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
+# resource "aws_dynamodb_table" "terraform-statelock-frontend" {
+#   name           = "terraform-statelock-frontend"
+#   hash_key       = "LockID"
+#   read_capacity  = 20
+#   write_capacity = 20
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
