1fe529e348581feeb02c7f014279752fe26ca814
Bo Chen <aaron027@Bos-MacBook-Air.gateway>
2022-11-12 18:24:39 +1100

    add eks tf

--- eks_tf/main.tf
+++ eks_tf/main.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "main" {
     security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
-    subnet_ids = aws_subnet.private_subnet.*.id
+    subnet_ids = concat(aws_subnet.private_subnet.*.id, aws_subnet.public_subnet.*.id)
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -71,7 +71,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = aws_subnet.private_subnet.*.id
+  subnet_ids      = concat(aws_subnet.private_subnet.*.id, aws_subnet.public_subnet.*.id)
 
   ami_type       = var.ami_type
   disk_size      = var.disk_size
@@ -97,34 +97,34 @@ resource "aws_eks_node_group" "main" {
 }
 
 # Nodes in public subnet
-resource "aws_eks_node_group" "public" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.node_group_name}-public"
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = aws_subnet.public_subnet.*.id
+# resource "aws_eks_node_group" "public" {
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = "${var.node_group_name}-public"
+#   node_role_arn   = aws_iam_role.eks_nodes.arn
+#   subnet_ids      = aws_subnet.public_subnet.*.id
 
-  ami_type       = var.ami_type
-  disk_size      = var.disk_size
-  instance_types = var.instance_types
+#   ami_type       = var.ami_type
+#   disk_size      = var.disk_size
+#   instance_types = var.instance_types
 
-  scaling_config {
-    desired_size = var.pblc_desired_size
-    max_size     = var.pblc_max_size
-    min_size     = var.pblc_min_size
-  }
+#   scaling_config {
+#     desired_size = var.pblc_desired_size
+#     max_size     = var.pblc_max_size
+#     min_size     = var.pblc_min_size
+#   }
 
-  tags = {
-    Name = "${var.node_group_name}-public"
-  }
+#   tags = {
+#     Name = "${var.node_group_name}-public"
+#   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.aws_eks_worker_node_policy,
-    aws_iam_role_policy_attachment.aws_eks_cni_policy,
-    aws_iam_role_policy_attachment.ec2_read_only,
-  ]
-}
+#   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+#   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+#   depends_on = [
+#     aws_iam_role_policy_attachment.aws_eks_worker_node_policy,
+#     aws_iam_role_policy_attachment.aws_eks_cni_policy,
+#     aws_iam_role_policy_attachment.ec2_read_only,
+#   ]
+# }
 
 resource "aws_iam_role" "eks_nodes" {
   name                 = "${var.eks_cluster_name}-worker"
